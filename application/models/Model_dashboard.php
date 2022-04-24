b0ef0ab8f9fbca27710e3db557c3b7c882be2fd6
user <ti.ervany@gmail.com>
2022-04-24 11:13:15 +0700

    fix chart dashboard

--- application/models/Model_dashboard.php
+++ application/models/Model_dashboard.php
@@ -11,7 +11,7 @@ class Model_dashboard extends CI_Model {
     }
 
 	public function get_penjualan_dashboard($tabel){
-        $tahun = date('Y');
+        $tahun = '2021';
         $penjualan = array(''.$tahun.'-01',''.$tahun.'-02',''.$tahun.'-03',''.$tahun.'-04',''.$tahun.'-05',''.$tahun.'-06',''.$tahun.'-07',''.$tahun.'-08',''.$tahun.'-09',''.$tahun.'-10',''.$tahun.'-11',''.$tahun.'-12');
             
         for($i=0; $i<count($penjualan); $i++){
